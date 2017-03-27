cd2d0bca086f24a84ff8e9e966fdd74974673899
mezz64 <jtmihalic@gmail.com>
2017-03-26 22:12:03 -0400

    Lay in structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2017 John Mihalic <https://github.com/mezz64>
+# Licensed under the MIT license.
+
+# Used this guide to create module
+# http://peterdowns.com/posts/first-time-with-pypi.html
+
+# git tag 0.1 -m "0.1 release"
+# git push --tags origin master
+#
+# Upload to PyPI Live
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
+
+
+from distutils.core import setup
+setup(
+    name='pyEight',
+    packages=['pyeight'],
+    version='0.0.1',
+    description='Provides a python api to interact with an Eight Sleep mattress cover.',
+    author='John Mihalic',
+    author_email='mezz64@users.noreply.github.com',
+    url='https://github.com/mezz64/pyEight',
+    download_url='https://github.com/mezz64/pyeight/tarball/0.0.1',
+    keywords=['eight', 'eightsleep', 'eight sleep', 'sleep', 'mattress', 'api wrapper', 'homeassistant'],
+    classifiers=[],
+    )
