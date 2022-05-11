c70dcbf8b90bba6c4ed7440d54353acf1d1b48d7
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-05-11 02:48:10 -0400

    Add final round of tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,20 @@
 """Provide common pytest fixtures."""
+import pathlib
 from unittest.mock import patch
 
-import aiohttp
-from aiohttp import test_utils, web
+from aiohttp import test_utils, web, ClientSession
 import pytest
 
-from . import load_fixture
 
-# pylint: disable=protected-access, unused-argument
+def load_fixture(name: str):
+    """Load a fixture."""
+    return (pathlib.Path(__file__).parent / "fixtures" / name).read_text()
 
 
 def handler_factory(filename: str):
     """Return a handler that returns a fixture."""
 
-    async def handler(request: web.Request):
+    async def handler(_: web.Request):
         return web.Response(
             headers={"content-type": "application/json"},
             text=load_fixture(f"{filename}.json"),
@@ -22,14 +23,13 @@ def handler_factory(filename: str):
     return handler
 
 
-@pytest.fixture(name="client_session")
-async def client_session_fixture(aiohttp_client):
+def _server(device_info: str, user_trends: str, user_intervals: str):
     app = web.Application()
     app.add_routes(
         [
             web.post("/login", handler_factory("login")),
             web.get("/users/me", handler_factory("logged_in_user_profile")),
-            web.get("/devices/{device_id}", handler_factory("device_info")),
+            web.get("/devices/{device_id}", handler_factory(device_info)),
             web.get(
                 "/users/389e711791e70fe16c54ce166ad8f3eb",
                 handler_factory("logged_in_user_profile"),
@@ -38,12 +38,30 @@ async def client_session_fixture(aiohttp_client):
                 "/users/e9e8a0c020a1159cc0549e189f6d4a15",
                 handler_factory("partner_user_profile"),
             ),
-            web.get("/users/{user_id}/trends", handler_factory("user_trends")),
-            web.get("/users/{user_id}/intervals", handler_factory("user_intervals")),
+            web.get("/users/{user_id}/trends", handler_factory(user_trends)),
+            web.get("/users/{user_id}/intervals", handler_factory(user_intervals)),
         ]
     )
-    server = test_utils.TestServer(app)
-    async with server, aiohttp.ClientSession() as session:
+    return test_utils.TestServer(app)
+
+
+@pytest.fixture(name="client_session")
+async def client_session_fixture():
+    async with _server(
+        "device_info", "user_trends", "user_intervals"
+    ) as server, ClientSession() as session:
+        new_url = f"http://localhost:{server.port}"
+        with patch("pyeight.eight.API_URL", new_url), patch(
+            "pyeight.user.API_URL", new_url
+        ):
+            yield session
+
+
+@pytest.fixture(name="client_session_missing_data")
+async def client_session_missing_data_fixture():
+    async with _server(
+        "device_info", "empty_user_trends", "empty_user_intervals"
+    ) as server, ClientSession() as session:
         new_url = f"http://localhost:{server.port}"
         with patch("pyeight.eight.API_URL", new_url), patch(
             "pyeight.user.API_URL", new_url
--- tests/test_user.py
+++ tests/test_user.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from pyeight.eight import EightSleep
+from pyeight.user import EightUser
 
 
 async def test_update_user_data(client_session):
@@ -14,7 +15,7 @@ async def test_update_user_data(client_session):
     await eight.update_device_data()
     await eight.update_user_data()
 
-    user = eight.users["e9e8a0c020a1159cc0549e189f6d4a15"]
+    user: EightUser = eight.users["e9e8a0c020a1159cc0549e189f6d4a15"]
 
     assert user.bed_presence is False
     assert user.target_heating_level == 0
@@ -33,19 +34,15 @@ async def test_update_user_data(client_session):
     assert user.current_session_date == datetime(
         2022, 3, 21, 19, 8, tzinfo=ZoneInfo("UTC")
     )
-    assert user.current_fitness_session_date == "2022-03-22"
     assert user.current_session_processing is False
     assert user.current_sleep_stage == "awake"
     assert user.current_sleep_score == 38
-    assert user.trend_sleep_score("2022-03-22") == 53
-    assert user.trend_sleep_score("2020-03-22") is None
-    assert user.sleep_fitness_score("2022-03-22") == 42
-    assert user.sleep_fitness_score("2020-03-22") is None
     assert user.current_sleep_fitness_score == 42
     assert user.current_sleep_duration_score == 0
     assert user.current_latency_asleep_score == 53
     assert user.current_latency_out_score == 100
     assert user.current_wakeup_consistency_score == 70
+    assert user.current_fitness_session_date == "2022-03-22"
     assert user.current_sleep_breakdown == {
         "awake": 4800,
         "deep": 1800,
@@ -70,8 +67,8 @@ async def test_update_user_data(client_session):
         "tnt": 3,
     }
     assert user.current_fitness_values == {
-        "asleep": 53,
         "date": "2022-03-22",
+        "asleep": 53,
         "duration": 0,
         "out": 100,
         "score": 42,
@@ -104,6 +101,10 @@ async def test_update_user_data(client_session):
         "score": 79,
         "tnt": 10,
     }
+    assert user.trend_sleep_score("2022-03-22") == 53
+    assert user.trend_sleep_score("2020-03-22") is None
+    assert user.sleep_fitness_score("2022-03-22") == 42
+    assert user.sleep_fitness_score("2020-03-22") is None
 
     assert user.past_heating_level(1) == 0
     # We should now have a past heating level
@@ -111,3 +112,36 @@ async def test_update_user_data(client_session):
     assert user.past_heating_level(1) == -32
 
     await eight.stop()
+
+
+async def test_update_user_data_missing_data(client_session_missing_data):
+    """Test update user data with missing data."""
+    eight = EightSleep(
+        "raman325@gmail.com",
+        "password",
+        "UTC",
+        client_session=client_session_missing_data,
+    )
+    await eight.start()
+    await eight.update_device_data()
+    await eight.update_user_data()
+
+    user = eight.users["e9e8a0c020a1159cc0549e189f6d4a15"]
+
+    assert user.bed_presence is False
+    assert user.current_session_date is None
+    assert user.current_session_processing is False
+    assert user.current_sleep_stage is None
+    assert user.current_sleep_score is None
+    assert user.current_sleep_fitness_score is None
+    assert user.current_sleep_duration_score is None
+    assert user.current_latency_asleep_score is None
+    assert user.current_latency_out_score is None
+    assert user.current_wakeup_consistency_score is None
+    assert user.current_fitness_session_date is None
+    assert user.current_sleep_breakdown is None
+    assert user.current_bed_temp is None
+    assert user.current_room_temp is None
+    assert user.current_tnt is None
+    assert user.current_resp_rate is None
+    assert user.current_heart_rate is None
